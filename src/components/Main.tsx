5d2b9a84eeded72ce728d6dab805346b97482f6e
sungaee <sungaee121@naver.com>
2026-07-27 18:49:47 +0900

    JSX로 UI표현

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,7 +1,11 @@
+import './Main.css'
+
 const Main = () => {
+    const number = 10;
   return (
     <main>
-      <h1>main</h1>
+      <h1 className="logout">main</h1>
+      <h2>{number}</h2>
     </main>
   )
 }
