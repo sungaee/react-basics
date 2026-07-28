347b505ebfb5ba4ae4ed4cca8704cda1a4a433f1
sungaee <sungaee121@naver.com>
2026-07-28 12:05:44 +0900

    State로 상태관리

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,26 @@
 import './App.css'
-import Button from './components/Button'
-
+import { useState } from 'react'
 function App() {
-  const buttonProps = {
-    text: '메일',
-    color: 'red',
-    a: 1,
-    b: 2,
-    c: 3,
-  }
+  const [count, setCount] = useState(0)
+  const [light, setLight] = useState("OFF")
   return (
     <>
-      <Button {...buttonProps} />
-      <Button text={'카페'} />
-      <Button text={'블로그'}>
-        <div>자식 요소</div> 
-        {/* 자식요소는 children이름의 프롭스로 전달된다. */}
-      </Button>
+    <div>
+      <h1>{light}</h1>
+      <button onClick={()=>{
+        setLight(light === 'ON' ? "OFF" : "ON")
+      }}>
+        {light === 'ON' ? "끄기" : "켜기"}
+      </button>
+    </div>
+    <div>
+      <h1>{count}</h1>
+      <button
+        onClick={() => {
+          setCount(count + 1)
+        }}
+      >+</button>
+      </div>
     </>
   )
 }
