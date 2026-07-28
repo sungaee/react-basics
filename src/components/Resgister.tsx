80ad87f9eeb08d5d5c777aa1434a0d61523966f7
sungaee <sungaee121@naver.com>
2026-07-28 14:55:33 +0900

    State로 하나의 통한된 이벤트 관리

--- src/components/Resgister.tsx
+++ src/components/Resgister.tsx
@@ -1,45 +1,50 @@
 import { useState } from 'react'
 const Register = () => {
-  const [name, setName] = useState('이름')
-  const [birth, setBirth] = useState("")
-  const [country, setCountry] = useState("")
-  const [bio, setBio] = useState("")
+  const [input, setInput] = useState({
+    name: '',
+    birth: '',
+    country: '',
+    bio: '',
+  })
 
-  //TODO ts 적용
-  const onChangeName = (e:any) => {
-    setName(e.target.value)
+  const onChange = (e: any) => {
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value,
+    })
   }
-  const onChangeBirth = (e:any) => {
-    setBirth(e.target.value)
-  }
-  const onChangeCountry = (e:any) => {
-    setCountry(e.target.value)
-  }
-  const onChangeBio=(e:any)=>{
-    setBio(e.target.value)
-  }
+
   return (
     <>
-    <div>
       <div>
-        <input value={name} onChange={onChangeName} placeholder={'이름'} />
+        <div>
+          <input
+            name="name"
+            value={input.name}
+            onChange={onChange}
+            placeholder={'이름'}
+          />
+        </div>
+        <div>
+          <input
+            name="birth"
+            value={input.birth}
+            onChange={onChange}
+            type="date"
+          />
+        </div>
       </div>
       <div>
-        <input value={birth} onChange={onChangeBirth} type="date" />
-      </div>
-    </div>
-    <div>
-        <select value={country} onChange={onChangeCountry}>
-            <option value=""></option>
-            <option value="kr">한국</option>
-            <option value="us">미국</option>
-            <option value="uk">영국</option>
+        <select name="country" value={input.country} onChange={onChange}>
+          <option value=""></option>
+          <option value="kr">한국</option>
+          <option value="us">미국</option>
+          <option value="uk">영국</option>
         </select>
-    </div>
-    <div>
-        <textarea value={bio} onChange={onChangeBio} />
-        {bio}
-    </div>
+      </div>
+      <div>
+        <textarea name="bio" value={input.bio} onChange={onChange} />
+      </div>
     </>
   )
 }
