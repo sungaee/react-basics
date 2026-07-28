07ec71ab18603c5b8266240ef42f5831de32c00e
sungaee <sungaee121@naver.com>
2026-07-28 11:49:08 +0900

    이벤트 처리

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,20 @@
-import type { ReactNode } from "react"
+import type { ReactNode } from 'react'
 interface ButtonProps {
   text: string
   color?: string
   children?: ReactNode
 }
 const Button = ({ children, text, color = 'black' }: ButtonProps) => {
+  const onClickButton = (e:any) => {
+    console.log(e);
+    console.log(text)
+  }
   return (
-    <button style={{ color }}>
+    <button
+      onClick={onClickButton}
+      //   onMouseEnter={onClickButton}
+      style={{ color }}
+    >
       {text} - {color.toUpperCase()}
       {children}
     </button>
