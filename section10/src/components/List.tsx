fa9a15951b9c9e89577035b7a45a4ff766b75a2c
sungaee <sungaee121@naver.com>
2026-08-30 18:07:46 +0900

    React Context 사용

---
+++ section10/src/components/List.tsx
@@ -0,0 +1,64 @@
+import './List.css'
+import TodoItem from './TodoItem'
+import { useState, useMemo, useContext } from 'react'
+import { TodoContext } from '../App'
+
+const List = () => {
+  const {todos} = useContext(TodoContext);
+  const [search, setSearch] = useState('')
+
+  const onChangeSearch = (e) => {
+    setSearch(e.target.value)
+  }
+
+  const getFilteredData = () => {
+    if (search === '') {
+      return todos
+    }
+    return todos.filter((todo) =>
+      todo.content.toLowerCase().includes(search.toLowerCase()),
+    )
+  }
+
+  const filteredTodos = getFilteredData()
+
+  const { totalCount, doneCount, notDoneCount } = useMemo(() => {
+    const totalCount = todos.length
+    const doneCount = todos.filter((todo) => todo.isDone).length
+    const notDoneCount = totalCount - doneCount
+
+    return {
+      totalCount,
+      doneCount,
+      notDoneCount,
+    }
+  }, [todos])
+  //의존성배열 : deps
+
+  return (
+    <div className="List">
+      <h4>Todo List 🌱</h4>
+      <div>
+        <div>total: {totalCount}</div>
+        <div>done: {doneCount}</div>
+        <div>notDone: {notDoneCount}</div>
+      </div>
+      <input
+        value={search}
+        onChange={onChangeSearch}
+        placeholder="검색어를 입력하세요"
+      />
+      <div className="todos_wrapper">
+        {filteredTodos.map((todo) => {
+          return (
+            <TodoItem
+              key={todo.id}
+              {...todo}
+            />
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+export default List
