19aa4824994ccc7a2be5b3b6c0bb5f83367171a5
sungaee <sungaee121@naver.com>
2026-09-03 16:14:21 +0900

    공통 컴포넌트 구현(Button, Header)

--- react-project/src/App.tsx
+++ react-project/src/App.tsx
@@ -5,31 +5,41 @@ import Diary from './pages/Diary'
 import New from './pages/New'
 import Notfound from './pages/Notfound'
 import { getEmotionImage } from './util/get-emotion-image'
+import Button from './components/Button'
+import Header from './components/Header'
 
 // 1. "/" : 모든 일기를 조회하는 Home페이지
 // 2. "/new" : 새로운 일기를 작성하는 New페이지
 // 3. "/diary" : 일기를 상세히 조회하는 Diary페이지
 function App() {
-  const nav = useNavigate()
-  const onClickButton = () => {
-    nav('/new')
-  }
-
   return (
     <>
-      <div>
-        <img src={getEmotionImage(1)} />
-        <img src={getEmotionImage(2)} />
-        <img src={getEmotionImage(3)} />
-        <img src={getEmotionImage(4)} />
-        <img src={getEmotionImage(5)} />
-      </div>
-      <div>
-        <Link to={'/'}>Home</Link>
-        <Link to={'/new'}>New</Link>
-        <Link to={'/diary'}>Diary</Link>
-      </div>
-      <button onClick={onClickButton}>New 페이지로 이동</button>
+      <Header
+        title={'Header'}
+        leftChild={<Button text={'Left'} />}
+        rightChild={<Button text={'Right'} />}
+      />
+      <Button
+        text={'뭉치'}
+        type={'DEFAULT'}
+        onClick={() => {
+          console.log('콜라')
+        }}
+      />
+      <Button
+        text={'뭉치'}
+        type={'POSITIVE'}
+        onClick={() => {
+          console.log('콜라')
+        }}
+      />
+      <Button
+        text={'뭉치'}
+        type={'NEGATIVE'}
+        onClick={() => {
+          console.log('콜라')
+        }}
+      />
 
       <Routes>
         <Route path="/" element={<Home />} />
