8395e007342eeae9ca2b36a76f3bf4c988a83526
sungaee <sungaee121@naver.com>
2026-09-07 18:37:12 +0900

    일기관리 기능 구현1

--- react-project/src/App.tsx
+++ react-project/src/App.tsx
@@ -1,50 +1,41 @@
 import './App.css'
+import { useReducer } from 'react'
 import { Routes, Route, Link, useNavigate } from 'react-router-dom'
 import Home from './pages/Home'
 import Diary from './pages/Diary'
 import New from './pages/New'
+import Edit from './pages/Edit'
 import Notfound from './pages/Notfound'
-import { getEmotionImage } from './util/get-emotion-image'
-import Button from './components/Button'
-import Header from './components/Header'
 
 // 1. "/" : 모든 일기를 조회하는 Home페이지
 // 2. "/new" : 새로운 일기를 작성하는 New페이지
 // 3. "/diary" : 일기를 상세히 조회하는 Diary페이지
+const mockData = [
+  {
+    id: 1,
+    createdDate: new Date().getTime(),
+    emotionId: 1,
+    content: '1번 일기내용',
+  },
+  {
+    id: 2,
+    createdDate: new Date().getTime(),
+    emotionId: 2,
+    content: '2번 일기내용',
+  },
+]
+function reducer(state, action) {
+  return state
+}
 function App() {
+  const [data, dispatch] = useReducer(reducer, mockData)
   return (
     <>
-      <Header
-        title={'Header'}
-        leftChild={<Button text={'Left'} />}
-        rightChild={<Button text={'Right'} />}
-      />
-      <Button
-        text={'뭉치'}
-        type={'DEFAULT'}
-        onClick={() => {
-          console.log('콜라')
-        }}
-      />
-      <Button
-        text={'뭉치'}
-        type={'POSITIVE'}
-        onClick={() => {
-          console.log('콜라')
-        }}
-      />
-      <Button
-        text={'뭉치'}
-        type={'NEGATIVE'}
-        onClick={() => {
-          console.log('콜라')
-        }}
-      />
-
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
         <Route path="/diary/:id" element={<Diary />} />
+        <Route path="/edit/:id" element={<Edit />} />
         <Route path="*" element={<Notfound />} />
       </Routes>
     </>
