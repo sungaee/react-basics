df58c9720cab08510488d6bb99bab72a2767f82d
sungaee <sungaee121@naver.com>
2026-09-08 18:33:54 +0900

    Home페이지 기능구현

--- react-project/src/components/DiaryItem.tsx
+++ react-project/src/components/DiaryItem.tsx
@@ -1,19 +1,30 @@
 import { getEmotionImage } from '../util/get-emotion-image'
 import Button from './Button'
 import './DiaryItem.css'
-const DiaryItem = () => {
-  const emotionId = 4
+import { useNavigate } from 'react-router-dom'
+
+const DiaryItem = ({ id, emotionId, createdDate, content }) => {
+  const nav = useNavigate()
 
   return (
     <div className="DiaryItem">
-      <div className={`img_section img_section_${emotionId}`}>
-        <img src={getEmotionImage(4)} alt="" />
+      <div
+        onClick={() => nav(`/diary/${id}`)}
+        className={`img_section img_section_${emotionId}`}
+      >
+        <img src={getEmotionImage(emotionId)} alt="" />
       </div>
-      <div className="info_section">
-        <div className="created_date">{new Date().toLocaleDateString()}</div>
-        <div className="content">일기 내용</div>
+      <div 
+      onClick={() => nav(`/diary/${id}`)}
+      className="info_section">
+        <div className="created_date">
+          {new Date(createdDate).toLocaleDateString()}
+        </div>
+        <div className="content">{content}</div>
       </div>
-      <div className="button_section">
+      <div 
+      onClick={()=>nav(`edit/${id}`)}
+      className="button_section">
         <Button text={'수정하기'} />
       </div>
     </div>
--- react-project/src/pages/Home.tsx
+++ react-project/src/pages/Home.tsx
@@ -1,14 +1,51 @@
-import Header from "../components/Header";
-import Button from "../components/Button";
-import DiaryList from "../components/DiaryList";
+import Header from '../components/Header'
+import Button from '../components/Button'
+import DiaryList from '../components/DiaryList'
+import { useState, useContext } from 'react'
+import { DiaryStateContext } from '../App'
 
-const Home = ()=>{
-    return <div>
-        <Header title={"2026년 9월"}
-        leftChild={<Button text={"<"}/>}
-        rightChild={<Button text={">"}/>}
-        />
-        <DiaryList />
-    </div>
+const getMonthlyData = (pivotDate, data) => {
+  const beginTime = new Date(
+    pivotDate.getFullYear(),
+    pivotDate.getMonth(),
+    1, //1일
+    0, //0시
+    0, //0분
+    0, //0초
+  ).getTime()
+
+  const endTime = new Date(
+    pivotDate.getFullYear(),
+    pivotDate.getMonth() + 1,
+    0, // 이전달의 마지막날 설정
+    23,
+    59,
+    59,
+  ).getTime()
+  return data.filter(
+    (item) => beginTime <= item.createdDate && item.createdDate <= endTime,
+  )
 }
-export default Home;
+const Home = () => {
+  const data = useContext(DiaryStateContext)
+  const [pivotDate, setPivotDate] = useState(new Date())
+  const monthlyData = getMonthlyData(pivotDate, data)
+
+  const onIncreaseMonth = () => {
+    setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() + 1))
+  }
+  const onDecreaseMonth = () => {
+    setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() - 1))
+  }
+  return (
+    <div>
+      <Header
+        title={`${pivotDate.getFullYear()}년 ${pivotDate.getMonth() + 1}월`}
+        leftChild={<Button onClick={onDecreaseMonth} text={'<'} />}
+        rightChild={<Button onClick={onIncreaseMonth} text={'>'} />}
+      />
+      <DiaryList data={monthlyData} />
+    </div>
+  )
+}
+export default Home
