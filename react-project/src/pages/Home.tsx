5992724d26ed32ec0809bf274f806e723bc5d73c
sungaee <sungaee121@naver.com>
2026-09-08 16:08:14 +0900

    Home페이지 UI구현

--- react-project/src/pages/Home.tsx
+++ react-project/src/pages/Home.tsx
@@ -1,4 +1,14 @@
+import Header from "../components/Header";
+import Button from "../components/Button";
+import DiaryList from "../components/DiaryList";
+
 const Home = ()=>{
-    return <div>Home</div>
+    return <div>
+        <Header title={"2026년 9월"}
+        leftChild={<Button text={"<"}/>}
+        rightChild={<Button text={">"}/>}
+        />
+        <DiaryList />
+    </div>
 }
 export default Home;
