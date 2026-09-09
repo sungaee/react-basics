0f4d18f18f5d235ccbb01e7297cb6a8d35c1078f
sungaee <sungaee121@naver.com>
2026-09-09 22:38:06 +0900

    New 페이지 UI구현

--- react-project/src/pages/New.tsx
+++ react-project/src/pages/New.tsx
@@ -1,4 +1,15 @@
-const New = ()=>{
-    return <div>New</div>
+import Header from '../components/Header'
+import Button from '../components/Button'
+import Editor from '../components/Editor'
+const New = () => {
+  return (
+    <div>
+      <Header
+        leftChild={<Button text={'< 뒤로 가기'} />}
+        title={'새 일기 쓰기'}
+      />
+      <Editor />
+    </div>
+  )
 }
-export default New;
+export default New
