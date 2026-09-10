10a2039ebb23c4c983e96d46aaaf644eaf479246
sungaee <sungaee121@naver.com>
2026-09-10 12:49:39 +0900

    New 페이지 기능구현

--- react-project/src/pages/New.tsx
+++ react-project/src/pages/New.tsx
@@ -1,14 +1,23 @@
 import Header from '../components/Header'
 import Button from '../components/Button'
 import Editor from '../components/Editor'
+import { useNavigate } from 'react-router-dom'
+import { useContext } from 'react'
+import { DiaryDispatchContext } from '../App'
 const New = () => {
+  const { onCreate } = useContext(DiaryDispatchContext)
+  const nav = useNavigate()
+  const onSubmit = (input) => {
+    onCreate(input.createdDate.getTime(), input.emotionId, input.content)
+    nav('/', { replace: true }) //홈으로 이동하면서 뒤로가기 방지
+  }
   return (
     <div>
       <Header
-        leftChild={<Button text={'< 뒤로 가기'} />}
+        leftChild={<Button onClick={() => nav(-1)} text={'< 뒤로 가기'} />}
         title={'새 일기 쓰기'}
       />
-      <Editor />
+      <Editor onSubmit={onSubmit} />
     </div>
   )
 }
