d0f9c31e3d728a6762480680e3ddb3835991d3ab
sungaee <sungaee121@naver.com>
2026-09-14 17:17:54 +0900

    Edit페이지 구현

--- react-project/src/components/Editor.tsx
+++ react-project/src/components/Editor.tsx
@@ -1,7 +1,7 @@
 import './Editor.css'
 import EmotionItem from './EmotionItem'
 import Button from './Button'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 const emotionList = [
@@ -39,13 +39,23 @@ const getStringedDate = (targetDate) => {
   }
   return `${year}-${month}-${date}`
 }
-const Editor = ({ onSubmit }) => {
+const Editor = ({ initData, onSubmit }) => {
   const [input, setInput] = useState({
     createdDate: new Date(),
     emotionId: 3,
     content: '',
   })
   const nav = useNavigate()
+
+  useEffect(() => {
+    if (initData) {
+      setInput({
+        ...initData,
+        createdDate: new Date(Number(initData.createdDate)),
+      })
+    }
+  }, [initData])
+
   const onChangeInput = (e) => {
     let name = e.target.name
     let value = e.target.value
