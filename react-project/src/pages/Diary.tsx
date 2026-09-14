46f2d2cd10a833628517b36c5f389ce9d5d2680c
sungaee <sungaee121@naver.com>
2026-09-14 22:03:09 +0900

    Diary페이지 구현

--- react-project/src/pages/Diary.tsx
+++ react-project/src/pages/Diary.tsx
@@ -1,8 +1,29 @@
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from 'react-router-dom'
+import Header from '../components/Header'
+import Button from '../components/Button'
+import Viewer from '../components/Viewer'
+import useDiary from '../hooks/useDiary'
+import { getStringedDate } from '../util/get-stringed-date'
 
-const Diary = ()=>{
+const Diary = () => {
+    const nav = useNavigate()
     const params = useParams()
-    
-    return <div>Diary</div>
+    const curDiaryItem = useDiary(params.id)
+    if(!curDiaryItem){
+        return <div>데이터 로딩중...!</div>
+    }
+    const {createdDate, emotionId, content} = curDiaryItem;
+    const title = getStringedDate(new Date(createdDate))
+
+  return (
+    <div>
+      <Header
+        leftChild={<Button onClick={()=> nav(-1)} text={'< 뒤로가기'} />}
+        title={`${title} 기록`}
+        rightChild={<Button onClick={()=>nav(`/edit/${params.id}`)} text={'수정하기'} />}
+      />
+      <Viewer emotionId={emotionId} content={content}/>
+    </div>
+  )
 }
-export default Diary;
+export default Diary
--- react-project/src/pages/Edit.tsx
+++ react-project/src/pages/Edit.tsx
@@ -2,25 +2,12 @@ import { useParams, useNavigate } from 'react-router-dom'
 import Header from '../components/Header'
 import Editor from '../components/Editor'
 import Button from '../components/Button'
-import { useContext, useEffect, useState } from 'react'
-import { DiaryDispatchContext, DiaryStateContext } from '../App'
+import useDiary from '../hooks/useDiary' 
 
 const Edit = () => {
   const params = useParams()
   const { onUpdate, onDelete } = useContext(DiaryDispatchContext)
-  const data = useContext(DiaryStateContext)
-  const [curDiaryItem, setCurDiaryItem] = useState()
-
-  useEffect(() => {
-    const currentDiaryItem = data.find(
-      (item) => String(item.id) === String(params.id),
-    )
-    if (!currentDiaryItem) {
-      window.alert('존재하지 않는 일기입니다.')
-      nav('/', { replace: true })
-    }
-    setCurDiaryItem(currentDiaryItem)
-  }, [params.id])
+    const curDiaryItem = useDiary(params.id)
 
   const nav = useNavigate()
   const onClickDelete = () => {
